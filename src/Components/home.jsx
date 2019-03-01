dba95af504d84b2e290d25c5ad9733037660d13b
anees rahiman <anees.syed@nagra.com>
2019-03-01 18:44:16 +0530

    update in code

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,30 +1,22 @@
 import React, { Component, createRef } from "react";
 import "../Css/styles.css";
 import "../Css/yellow.css";
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
-const GenerateRoles = (prop) => {
-    return (
-        <b className={prop.bClass}>{prop.roles}</b>
-    )
-}
 class Home extends Component {
     constructor(props) {
         super(props);
         this.myRef = createRef();
-        this.next = 0;
+        this.index = 0;
         this.state = {
             roles : [
                 "developer",
                 "designer",
                 "freelancer"
-            ]
+            ],
+            role : "developer"
         }
     }
     render (){
-        const roleItems = this.state.roles.map((role, index) => {
-            return <GenerateRoles key={role} roles={role} />
-        });
         return (
             <section id="home" className="active">
                 <div className="text-rotator-container">
@@ -33,16 +25,27 @@ class Home extends Component {
                             HI ! i'm 
                             <span> Anees</span>
                         </h1>
-                        <h2 className="cd-headline clip" >
+                        <h2>
                             I'm a
-                            <ReactCSSTransitionGroup>
-                                {roleItems}
-                            </ReactCSSTransitionGroup>
+                            <span className="roles">
+                                <b>{this.state.role}</b>
+                                <span></span>
+                            </span>
                         </h2>
                     </div>
                 </div>
             </section>
         )
+    }
+
+    componentDidMount () {
+        setInterval(() => {
+            let role = this.state.roles[this.index];
+            this.index = (this.index === this.state.roles.length - 1) ? 0 : this.index + 1; 
+            this.setState(() => ({
+                role: role
+            }));
+        }, 3000);
     }
 }
 
