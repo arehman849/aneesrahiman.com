6092f5458f655c0815729b565ce8c3b511fd5738
anees rahiman <anees.syed@nagra.com>
2019-03-08 16:34:29 +0530

    adding more of about page implementation

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { HashRouter, NavLink as NavigationLink} from "react-router-dom";
 import "../Css/styles.css";
 import "../Css/yellow.css";
 import FaIcon from "./icons";
@@ -22,6 +23,38 @@ class About extends Component {
         for(let [key, value] of Object.entries(personalDetails.personal2)) {
             personalItems2.push(<GenerateList key={key} dispKey={key} value={value}></GenerateList>)
         }
+        const resumeHeaders = resumeCardsTitles.map((title, index) => {
+            let id = "resume-list-item-" + index;
+            return (
+                <div className="resume-list-item" key={title}  data-index={index} id={id} onClick={this.setActive}>
+                    <div className="resume-list-item-inner">
+                        <h6 className="resume-list-item-title uppercase">
+                            <FaIcon key={title} value={title}/> &nbsp;
+                            {title}
+                        </h6>
+                    </div>
+                </div>
+            )
+        })
+        const resumeCards = resumeCardsTitles.map((title, index) => {
+            let resumeCardIndexCls = "resume-card resume-card-" + index;
+            let resumeBodyClass = "resume-card-body " + title;
+            return (
+                <div className={resumeCardIndexCls} data-index={index} key={resumeCardIndexCls}  onClick={this.setActive}>
+                    <div className="resume-card-header">
+                        <div className="resume-card-name">
+                            <FaIcon key={title} value={title} /> &nbsp; 
+                            {title}
+                        </div>
+                    </div>
+                    <div className={resumeBodyClass}>
+                        <div className="resume-card-body-container">
+
+                        </div>
+                    </div>
+                </div>
+            )
+        })
         return (
         <section id="About" className="active">
             <div className="container page-title custom-page-title">
@@ -58,11 +91,62 @@ class About extends Component {
                                 {personalItems2}
                             </ul>
                         </div>
+                        <div className="col s12 m12 l12 p-none call-to-actions">
+                            <HashRouter>
+                                <div>
+                                    <NavigationLink to="About" id="link-About" className="btn font-weight-700">
+                                        <FaIcon key="Resume" value="Resume" /> &nbsp;
+                                        My Resume
+                                    </NavigationLink>
+                                    <NavigationLink to="Blog" id="link-Blog" onClick={this.clickHandler} className="btn btn-secondary font-weight-700">
+                                        <FaIcon key="Blog" value="Blog" /> &nbsp;
+                                        My Blog
+                                    </NavigationLink>
+                                </div>
+                            </HashRouter>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="resume-container">
+                <div className="container section-padding">
+                    <div className="valign-wrapper row">
+                        <div className="resume-list col m5 l4 section-padding">
+                            {resumeHeaders}
+                        </div>
+                        <div className="col s12 m7 l8 resume-cards-container section-padding">
+                            <div className="resume-cards">
+                                {resumeCards}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
         )
+    }
+
+    componentDidMount () {
+        document.getElementById("link-About").parentNode.className = "active";
+        document.getElementById("resume-list-item-0").className += " is-active";
+    }
+    clickHandler (e) {
+        var nav = document.getElementById("nav");
+        nav.childNodes.forEach(element => {
+            element.className = "";  
+            if(e.target.id === element.firstChild.id) {
+                element.className = "active";
+            }         
+        });
+    }
+
+    setActive (e) {
+        Array.from(document.getElementsByClassName("resume-list-item")).forEach(function(link) {
+            link.className = "resume-list-item"
+            if(link.id === e.currentTarget.id) {
+                link.className = "resume-list-item is-active";
+            }
+        });
     }
 }
 
@@ -80,4 +164,6 @@ const personalDetails = {
         Languages: "English, Hindi, Kannada"
     }
 }
+
+const resumeCardsTitles = ["experience", "education", "skills"];
 export default About;
--- src/Components/icons.jsx
+++ src/Components/icons.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faUser, faBriefcase, faComment, faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faBriefcase, faComment, faEnvelopeOpen, faGraduationCap, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faFilePdf } from "@fortawesome/free-regular-svg-icons";
+// import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-library.add(faHome, faUser, faBriefcase, faComment, faEnvelopeOpen);
+library.add(faHome, faUser, faBriefcase, faComment, faEnvelopeOpen, faFilePdf, faGraduationCap, faStar);
 
 const FaIcon = (prop) => {
     return (
@@ -21,6 +23,7 @@ const getIcon = (item) => {
             icon = "user"
             break;
         case "Work":
+        case "experience":
             icon = "briefcase"
             break;
         case "Blog":
@@ -28,6 +31,15 @@ const getIcon = (item) => {
             break;
         case "Contact":
             icon = "envelope-open"
+            break;
+        case "Resume":
+            icon = {prefix: 'far', iconName: 'file-pdf'}
+            break;
+        case "education":
+            icon = "graduation-cap"
+            break;
+        case "skills":
+            icon = "star"
             break;
         default:
             break;
